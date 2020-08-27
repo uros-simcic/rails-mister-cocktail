95ecb98185727c764a2fda77ae9032c132d6f3d2
uros simcic <inbox.si@gmail.com>
2020-08-27 14:16:58 -0300

    commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+// app/javascript/packs/application.js
+import 'bootstrap';
+
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
