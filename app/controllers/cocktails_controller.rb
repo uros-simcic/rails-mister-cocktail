ec5d80fde22a0b4cc24d8df64f2ba54862564040
uros simcic <inbox.si@gmail.com>
2020-08-31 08:10:06 -0300

    commit

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,6 +10,14 @@
     @cocktail = Cocktail.new
   end
 
+  def create
 
+    @cocktail = Cocktail.new(cocktail_params)
+
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
 end
 
